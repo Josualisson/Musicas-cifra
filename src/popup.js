74e5beda63aeacea7160409e8d359f6fe498e1a8
Josué Alisson <bateriadotransformice@gmail.com>
2022-06-29 01:06:00 -0300

    teste

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,4 @@
+/* POPUP 1 */
 function iniciaModal(modalID) {
 			
     const modal = document.getElementById(modalID);
@@ -13,6 +14,8 @@ function iniciaModal(modalID) {
 }
 const logo = document.querySelector('.click');
 logo.addEventListener('click', () => iniciaModal('modal-promocao')); //mouseover
+
+/* POPUP 2 */
 
 function iniciaModal3(modalID) {
 			
@@ -31,3 +34,45 @@ function iniciaModal3(modalID) {
 
 const logo3 = document.querySelector('.clicar');
 logo3.addEventListener('click', () => iniciaModal3('mostrar-modal')); //mouseover
+
+
+/* pop up 3 */
+
+function iniciaModal4(modalID) {
+			
+    const modal4 = document.getElementById(modalID);
+    if(modal4) {
+        modal4.classList.add('mostrar4');
+        modal4.addEventListener('click', (e) => {
+            if(e.target.id == modalID || e.target.className == 'fechar4') {
+                modal4.classList.remove('mostrar4');
+                
+            }
+        });
+    }
+
+}
+
+const logo4 = document.querySelector('.clicar4');
+logo4.addEventListener('click', () => iniciaModal4('mostrar-modal4')); //mouseover
+
+/* POPUP 4 */
+
+function iniciaModal5(modalID) {
+			
+    const modal5 = document.getElementById(modalID);
+    if(modal5) {
+        modal5.classList.add('mostrar5');
+        modal5.addEventListener('click', (e) => {
+            if(e.target.id == modalID || e.target.className == 'fechar5') {
+                modal5.classList.remove('mostrar5');
+                
+            }
+        });
+    }
+
+}
+
+const logo5 = document.querySelector('.clicar5');
+logo5.addEventListener('click', () => iniciaModal5('mostrar-modal5')); //mouseover
+
